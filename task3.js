5f3a3f7062560cfb726ba07a1228c4d789ea86cd
Nurzat <nurzat.izakanova.ss@gmail.com>
2022-01-12 12:10:42 +0600

    add task3.js

---
+++ task3.js
@@ -0,0 +1,6 @@
+// 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let userName = "Nur";
+document.write(userName + "br");
+let postalAddress = "72000, Кыргызстан, г.Бишкек";
+document.write(postalAddress + "br");
