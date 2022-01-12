82e8e33b79287e4cfe6e0a8004df076242997a10
Nurzat <nurzat.izakanova.ss@gmail.com>
2022-01-12 17:09:20 +0600

    add script4.js

---
+++ script4.js
@@ -0,0 +1,7 @@
+let souvenir = 112;
+let trinket = 75;
+let a;
+a = prompt("Введите количество сувениров?");
+let b;
+b = prompt("Введите количество безделушек?");
+let result = souvenir * a + trinket * b;
